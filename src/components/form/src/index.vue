ffaa97bea345ca268dbcb6b95734e15472a855d0
张运斌 <zhangyunbin@MacBook-Air.local>
2023-04-09 14:59:51 +0800

    first commit

---
+++ src/components/form/src/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <el-form ref="form" v-if="model" :validate-on-rule-change="false" :model="model" :rules="rules" v-bind="$attrs"
+    :label-position="labelPosition" :label-width="labelWidth">
+    <el-row :gutter="gutter">
+      <template v-for="(item, index) in options" :key="index">
+        <el-col :span="item.span || colSpan" v-if="!item.children || !item.children!.length">
+          <el-form-item v-bind="item">
+            <component v-if="item.type !== 'upload' && item.type !== 'editor'" :placeholder="item.placeholder"
+              v-bind="item.attrs" :is="`el-${item.type}`" v-model="model[item.prop!]"></component>
+          </el-form-item>
+        </el-col>
+        <el-col v-if="item.children && item.children.length" :span="item.span || colSpan">
+          <el-form-item :prop="item.prop" :label="item.label">
+            <component :placeholder="item.placeholder" v-bind="item.attrs" :is="`el-${item.type}`"
+              v-model="model[item.prop!]">
+              <component v-for="(child, i) in item.children" :key="i" :is="`el-${child.type}`" :label="child.label"
+                :value="child.value"></component>
+            </component>
+          </el-form-item>
+        </el-col>
+      </template>
+      <el-col v-if="custom" :span="colSpan">
+        <slot name="custom" :form="form" :model="model" />
+      </el-col>
+      <el-form-item :style="{ marginLeft: gutter / 2 + 'px' }">
+        <slot name="action" :form="form" :model="model" class="action" />
+      </el-form-item>
+    </el-row>
+    <slot name="default" :form="form" :model="model" />
+  </el-form>
+</template>
+
+<script lang='ts' setup>
+import { PropType, ref, onMounted, watch, computed, useSlots } from 'vue'
+import { FormInstance, FormOptions, styleObj } from './types/types'
+import cloneDeep from 'lodash/cloneDeep'
+
+const props = defineProps({
+  // 表单的配置项
+  options: {
+    type: Array as PropType<FormOptions[]>,
+    required: true
+  },
+  // 行内表单
+  inline: {
+    type: Boolean,
+    default: true
+  },
+  labelPosition: {
+    type: String,
+    default: 'right'
+  },
+  cows: {
+    type: Number,
+    default: 4
+  },
+  gutter: {
+    type: Number,
+    default: 20
+  },
+  actionPosition: {
+    type: String,
+    default: 'right'
+  },
+  labelWidth: {
+    type: String,
+    default: '80px'
+  }
+
+})
+
+const model = ref<any>(null)
+const rules = ref<any>(null)
+const form = ref<FormInstance | null>()
+
+// 初始化表单
+const initForm = () => {
+  if (props.options && props.options.length) {
+    let m: any = {}
+    let r: any = {}
+    props.options.map((item: FormOptions) => {
+      item.span = props.inline ? item.span : 24
+      m[item.prop!] = item.value
+      r[item.prop!] = item.rules
+    })
+    model.value = cloneDeep(m)
+    rules.value = cloneDeep(r)
+  }
+}
+
+// 重置表单
+const resetFields = () => {
+  form.value!.resetFields()
+}
+
+// 表单验证方法
+const validate = (callback: () => {}) => form.value!.validate(callback)
+
+// 暴露出去给父组件使用
+defineExpose({ resetFields, validate })
+
+// 生命周期
+onMounted(() => {
+  initForm()
+})
+
+// 计算el-col组件的span值
+const colSpan = computed(() => {
+  return (parseInt(24 as any / props.cows as any) <= 6) ? 6 : (parseInt(24 as any / props.cows as any))
+})
+
+// 设置按钮位置
+const actionBtnPosition = computed(() => {
+  const styleObj: styleObj = {}
+
+  styleObj.paddingLeft = (props.gutter / 2) + 'px'
+  if (props.actionPosition === 'right') {
+    styleObj.marginRight = (props.gutter / 2) + 'px'
+  }
+
+  return styleObj
+})
+
+// 监听父组件传递进来的options
+watch(() => props.options, () => {
+  initForm()
+}, { deep: true })
+
+
+const { custom } = useSlots()
+</script>
+
+<style lang='scss' scoped>
+.el-form-item {
+  margin-bottom: 24px;
+}
+</style>
---
+++ src/views/form/index.vue
@@ -0,0 +1,217 @@
+<template>
+  <div>
+    <m-form ref="form" :options="options" label-position="left" :cows="5" action-position="right">
+      <template #action="scope">
+        <el-button type="primary" @click="submitForm">提交</el-button>
+        <el-button @click="resetForm">重置</el-button>
+      </template>
+      <template #custom="scope">
+        <el-form-item label="测试">
+          <input />
+        </el-form-item>
+      </template>
+      <template #default="scope">
+        <div>1</div>
+      </template>
+    </m-form>
+  </div>
+</template>
+
+<script lang='ts' setup>
+import { FormInstance, FormOptions } from '../../components/form/src/types/types';
+import { ElMessage } from 'element-plus'
+import { ref } from 'vue'
+
+interface Scope {
+  form: FormInstance,
+  model: any
+}
+
+const options: FormOptions[] = [
+  {
+    type: 'input',
+    value: '',
+    label: '用户名',
+    prop: 'username',
+    placeholder: '请输入用户名',
+    rules: [
+      {
+        required: true,
+        message: '用户名不能为空',
+        trigger: 'blur'
+      },
+      {
+        min: 2,
+        max: 6,
+        message: '用户名在2-6位之间',
+        trigger: 'blur'
+      }
+    ],
+    attrs: {
+      clearable: true,
+    }
+  },
+  {
+    type: 'input',
+    value: '',
+    label: '密码',
+    prop: 'password',
+    placeholder: '请输入密码',
+    rules: [
+      {
+        required: true,
+        message: '密码不能为空',
+        trigger: 'blur'
+      },
+      {
+        min: 6,
+        max: 15,
+        message: '密码在6-15位之间',
+        trigger: 'blur'
+      }
+    ],
+    attrs: {
+      showPassword: true,
+      clearable: true
+    }
+  },
+  {
+    type: 'select',
+    value: '',
+    placeholder: '请选择职位',
+    prop: 'role',
+    label: '职位',
+    attrs: {
+      style: {
+        width: '100%'
+      },
+    },
+    rules: [
+      {
+        required: true,
+        message: '职位不能为空',
+        trigger: 'change'
+      }
+    ],
+    children: [
+      {
+        type: 'option',
+        label: '经理',
+        value: '1'
+      },
+      {
+        type: 'option',
+        label: '主管',
+        value: '2'
+      },
+      {
+        type: 'option',
+        label: '员工',
+        value: '3'
+      }
+    ]
+  },
+  {
+    type: 'input',
+    value: '',
+    label: '用户名',
+    prop: 'username',
+    placeholder: '请输入用户名',
+    rules: [
+      {
+        required: true,
+        message: '用户名不能为空',
+        trigger: 'blur'
+      },
+      {
+        min: 2,
+        max: 6,
+        message: '用户名在2-6位之间',
+        trigger: 'blur'
+      }
+    ],
+    attrs: {
+      clearable: true
+    }
+  },
+  {
+    type: 'time-picker',
+    prop: 'time',
+    value: [],
+    label: '时间',
+    attrs: {
+      'is-range': true,
+      'range-separator': 'To',
+      'start-placeholder': 'Start time',
+      'end-placeholder': 'End time'
+    },
+    rules: [
+      {
+        required: true,
+        message: '用户名不能为空',
+        trigger: 'blur'
+      }
+    ]
+  },
+  {
+    type: 'select',
+    value: '',
+    placeholder: '请选择职位',
+    prop: 'woker',
+    label: '职位',
+    attrs: {
+      style: {
+        width: '100%'
+      },
+    },
+    rules: [
+      {
+        required: true,
+        message: '职位不能为空',
+        trigger: 'change'
+      }
+    ],
+    children: [
+      {
+        type: 'option',
+        label: '经理',
+        value: '1'
+      },
+      {
+        type: 'option',
+        label: '主管',
+        value: '2'
+      },
+      {
+        type: 'option',
+        label: '员工',
+        value: '3'
+      }
+    ]
+  },
+  {
+    type: 'color-picker',
+    value: '#000',
+    prop: 'color',
+    label: '评分',
+  }
+]
+
+const form = ref()
+
+// 提交表单
+const submitForm = (scope: Scope) => {
+  form.value.validate((valid: boolean) => {
+    if (valid) {
+      ElMessage.success('提交成功')
+    } else {
+      ElMessage.error('表单填写有误,请检查')
+    }
+  })
+}
+
+// 重置表单
+const resetForm = () => form.value.resetFields()
+</script>
+
+<style lang='scss' scoped></style>
